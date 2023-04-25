382519db568e1b07ad3cc01f0cd760f73b49da69
github-action-benchmark <github@users.noreply.github.com>
2023-04-25 20:53:24 +0000

    add Benchmark (cargo) benchmark result for 6c921a0cc0f86f82c114cfd21b5b24afa67f07e0

---
+++ bench/indexer/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1682456004352,
+  "repoUrl": "https://github.com/MystenLabs/sui",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "lxfind@gmail.com",
+            "name": "Xun Li",
+            "username": "lxfind"
+          },
+          "committer": {
+            "email": "lxfind@gmail.com",
+            "name": "Xun Li",
+            "username": "lxfind"
+          },
+          "distinct": true,
+          "id": "6c921a0cc0f86f82c114cfd21b5b24afa67f07e0",
+          "message": "Fix release build",
+          "timestamp": "2023-04-25T13:42:31-07:00",
+          "tree_id": "e1deae07393a28388c87f4863aa4a2b1a94ba5ac",
+          "url": "https://github.com/MystenLabs/sui/commit/6c921a0cc0f86f82c114cfd21b5b24afa67f07e0"
+        },
+        "date": 1682456001641,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "persist_checkpoint",
+            "value": 143989111,
+            "range": "± 4682497",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "get_checkpoint",
+            "value": 325357,
+            "range": "± 27396",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
