ff23cc9002c8878044ae31a00f5864abf69cf71a
tzakian <tzakian@users.noreply.github.com>
2023-04-26 19:28:51 +0000

    deploy: ec85b4a46b4a9c74155aef18ffd99dd4c0bc3cfe

--- crates/transaction_fuzzer/type_arg_fuzzer/sidebar-items.js
+++ crates/transaction_fuzzer/type_arg_fuzzer/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["gen_nested_type_tag","gen_struct_tag","gen_type_tag","pt_for_tags","run_type_tags"]};
+window.SIDEBAR_ITEMS = {"fn":["base_type_factory_tag_gen","gen_nested_type_tag","gen_struct_tag","gen_type_tag","generate_valid_and_invalid_type_factory_tags","generate_valid_type_factory_tags","nested_type_factory_tag_gen","pt_for_tags","run_pt","type_factory_pt_for_tags"]};
