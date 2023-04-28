9bc2b9c5b1822be277daf24ddae7023c5450e341
lxfind <lxfind@users.noreply.github.com>
2023-04-28 00:39:58 +0000

    deploy: 0aacca3192f9e17016dae4e38171f35e4fa7f215

--- crates/implementors/sui_config/trait.Config.js
+++ crates/implementors/sui_config/trait.Config.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "sui_config":[],
-"sui_sdk":[["impl <a class=\"trait\" href=\"sui_config/trait.Config.html\" title=\"trait sui_config::Config\">Config</a> for <a class=\"struct\" href=\"sui_sdk/sui_client_config/struct.SuiClientConfig.html\" title=\"struct sui_sdk::sui_client_config::SuiClientConfig\">SuiClientConfig</a>"]]
+"sui_sdk":[["impl Config for <a class=\"struct\" href=\"sui_sdk/sui_client_config/struct.SuiClientConfig.html\" title=\"struct sui_sdk::sui_client_config::SuiClientConfig\">SuiClientConfig</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
