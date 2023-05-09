c5bd70e9a65fe1041465c9ed3fe3af0d94d13981
lxfind <lxfind@users.noreply.github.com>
2023-05-09 16:56:49 +0000

    deploy: 84862080feb6b3e29e405f2a609836a64cf613e3

--- crates/sui_proxy/middleware/sidebar-items.js
+++ crates/sui_proxy/middleware/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["expect_mysten_proxy_header","expect_valid_public_key"],"struct":["LenDelimProtobuf"]};
+window.SIDEBAR_ITEMS = {"fn":["expect_content_length","expect_mysten_proxy_header","expect_valid_public_key"],"struct":["LenDelimProtobuf"]};
