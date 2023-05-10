8939a93f0ca0e29a577d93c30894330c8ea41f12
gegaowp <gegaowp@users.noreply.github.com>
2023-05-10 21:50:45 +0000

    deploy: 23a212db97f6b8a5395dc8d7bbbf90bc26e8e106

--- crates/sui_indexer/types/sidebar-items.js
+++ crates/sui_indexer/types/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["CheckpointTransactionBlockResponse","FastPathTransactionBlockResponse","SuiTransactionBlockResponseWithOptions","TemporaryTransactionBlockResponseStore"]};
+window.SIDEBAR_ITEMS = {"struct":["AddressData","CheckpointTransactionBlockResponse","FastPathTransactionBlockResponse","SuiTransactionBlockResponseWithOptions","TemporaryTransactionBlockResponseStore"]};
