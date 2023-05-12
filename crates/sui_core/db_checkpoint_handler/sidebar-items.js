59c9d4e24583e599464a12efa7c50ae0cfc51997
sadhansood <sadhansood@users.noreply.github.com>
2023-05-12 21:12:49 +0000

    deploy: 5ec0adc3b669e2e8e4f44f14c86963c2e11db522

--- crates/sui_core/db_checkpoint_handler/sidebar-items.js
+++ crates/sui_core/db_checkpoint_handler/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["SUCCESS_MARKER","TEST_MARKER","UPLOAD_COMPLETED_MARKER"],"struct":["DBCheckpointHandler"]};
+window.SIDEBAR_ITEMS = {"constant":["SUCCESS_MARKER","TEST_MARKER","UPLOAD_COMPLETED_MARKER"],"struct":["DBCheckpointHandler","DBCheckpointMetrics"]};
