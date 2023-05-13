0602731bea9a98f4a4421eefd885ab2f6244506a
emmazzz <emmazzz@users.noreply.github.com>
2023-05-13 20:00:22 +0000

    deploy: f6494ee216e7fdc6de8ddc21d9a7cae86e3255c9

--- crates/test_utils/transaction/sidebar-items.js
+++ crates/test_utils/transaction/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["increment_counter","make_publish_package","parse_package_ref","publish_basics_package_and_make_counter","publish_counter_package","publish_package","submit_shared_object_transaction","submit_single_owner_transaction","transfer_coin","wait_for_all_txes","wait_for_tx"]};
+window.SIDEBAR_ITEMS = {"fn":["parse_package_ref","publish_counter_package","publish_package","submit_shared_object_transaction","submit_single_owner_transaction","wait_for_all_txes","wait_for_tx"]};
