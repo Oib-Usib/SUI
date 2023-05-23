0956b131b3289573f5f72d07441213104444b0c2
github-action-benchmark <github@users.noreply.github.com>
2023-05-23 04:27:00 +0000

    add Benchmark (cargo) benchmark result for 5fd9a2e73c6505e4d51a488b0c587580c1a50e1d

--- bench/indexer/data.js
+++ bench/indexer/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1684800272177,
+  "lastUpdate": 1684816019731,
   "repoUrl": "https://github.com/MystenLabs/sui",
   "entries": {
     "Benchmark": [
@@ -9323,6 +9323,42 @@ window.BENCHMARK_DATA = {
             "name": "get_checkpoint",
             "value": 307746,
             "range": "± 19695",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "8418040+longbowlu@users.noreply.github.com",
+            "name": "Lu Zhang",
+            "username": "longbowlu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5fd9a2e73c6505e4d51a488b0c587580c1a50e1d",
+          "message": "move metered-channel to mysten-metrics (#12156)\n\n## Description \r\n\r\nThis PR moves metered-channel from narwhal to mysten-metrics and uses it\r\non `streamer` to monitor the channel size.\r\n\r\n## Test Plan \r\n\r\nTested it locally and see the metrics appeared.\r\n\r\n---\r\nIf your changes are not user-facing and not a breaking change, you can\r\nskip the following section. Otherwise, please indicate what changed, and\r\nthen add to the Release Notes section as highlighted during the release\r\nprocess.\r\n\r\n### Type of Change (Check all that apply)\r\n\r\n- [ ] user-visible impact\r\n- [ ] breaking change for a client SDKs\r\n- [ ] breaking change for FNs (FN binary must upgrade)\r\n- [ ] breaking change for validators or node operators (must upgrade\r\nbinaries)\r\n- [ ] breaking change for on-chain data layout\r\n- [ ] necessitate either a data wipe or data migration\r\n\r\n### Release notes\r\nMoves metered-channel from narwhal to mysten-metrics and uses it on\r\n`streamer` to monitor the channel size.",
+          "timestamp": "2023-05-23T04:13:07Z",
+          "tree_id": "e7beb820279f0d439552699037df20259c1e7f2c",
+          "url": "https://github.com/MystenLabs/sui/commit/5fd9a2e73c6505e4d51a488b0c587580c1a50e1d"
+        },
+        "date": 1684816002615,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "persist_checkpoint",
+            "value": 146607214,
+            "range": "± 4258745",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "get_checkpoint",
+            "value": 325977,
+            "range": "± 19434",
             "unit": "ns/iter"
           }
         ]
