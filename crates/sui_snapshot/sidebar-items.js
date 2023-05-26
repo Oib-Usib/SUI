2f023d42bdc40b183cc4ccc3af8b3d64cd830a3d
sadhansood <sadhansood@users.noreply.github.com>
2023-05-26 00:31:10 +0000

    deploy: a7e8872a90a3d4e85057d29392482d5378be5f9c

--- crates/sui_snapshot/sidebar-items.js
+++ crates/sui_snapshot/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Encoding","FileCompression","FileType","Manifest"],"fn":["compute_sha3_checksum","compute_sha3_checksum_for_file","create_file_metadata"],"struct":["Blob","FileMetadata","ManifestV1"]};
+window.SIDEBAR_ITEMS = {"enum":["Encoding","FileCompression","FileType","Manifest"],"fn":["compute_sha3_checksum","compute_sha3_checksum_for_file","create_file_metadata","setup_db_state"],"struct":["Blob","FileMetadata","ManifestV1"]};
