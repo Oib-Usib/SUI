48fbca24f890a323eab56947e7d4b1304ef34272
arun-koshy <arun-koshy@users.noreply.github.com>
2023-05-30 18:46:00 +0000

    deploy: 59e0f091833a0cef7761bd12274180b1fb9d7d53

--- crates/implementors/sui_config/trait.Config.js
+++ crates/implementors/sui_config/trait.Config.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "sui_config":[],
 "sui_sdk":[["impl <a class=\"trait\" href=\"sui_config/trait.Config.html\" title=\"trait sui_config::Config\">Config</a> for <a class=\"struct\" href=\"sui_sdk/sui_client_config/struct.SuiClientConfig.html\" title=\"struct sui_sdk::sui_client_config::SuiClientConfig\">SuiClientConfig</a>"]],
-"sui_swarm_config":[["impl Config for <a class=\"struct\" href=\"sui_swarm_config/genesis_config/struct.GenesisConfig.html\" title=\"struct sui_swarm_config::genesis_config::GenesisConfig\">GenesisConfig</a>"],["impl Config for <a class=\"struct\" href=\"sui_swarm_config/network_config/struct.NetworkConfig.html\" title=\"struct sui_swarm_config::network_config::NetworkConfig\">NetworkConfig</a>"]]
+"sui_swarm_config":[["impl Config for <a class=\"struct\" href=\"sui_swarm_config/network_config/struct.NetworkConfig.html\" title=\"struct sui_swarm_config::network_config::NetworkConfig\">NetworkConfig</a>"],["impl Config for <a class=\"struct\" href=\"sui_swarm_config/genesis_config/struct.GenesisConfig.html\" title=\"struct sui_swarm_config::genesis_config::GenesisConfig\">GenesisConfig</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
