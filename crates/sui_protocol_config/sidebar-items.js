dc8d66d39a5b4279ee2e595a1620e56b048540a7
williampsmith <williampsmith@users.noreply.github.com>
2023-06-01 03:04:29 +0000

    deploy: e8a351ecf791793eb00f5e986b8a0eb35df532d6

--- crates/sui_protocol_config/sidebar-items.js
+++ crates/sui_protocol_config/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["LimitThresholdCrossed","ProtocolConfigValue"],"fn":["check_limit_in_range"],"macro":["check_limit","check_limit_by_meter"],"struct":["Error","OverrideGuard","ProtocolConfig","ProtocolVersion","SupportedProtocolVersions"]};
+window.SIDEBAR_ITEMS = {"enum":["Chain","LimitThresholdCrossed","ProtocolConfigValue"],"fn":["check_limit_in_range"],"macro":["check_limit","check_limit_by_meter"],"struct":["Error","OverrideGuard","ProtocolConfig","ProtocolVersion","SupportedProtocolVersions"]};
