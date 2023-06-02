350a04b8731aa68aecf194ddbfc169f413da3eb4
mystenmark <mystenmark@users.noreply.github.com>
2023-06-02 17:05:20 +0000

    deploy: e52af128605b4328695eebc4c3e51b635457712c

--- crates/sui_core/authority/authority_per_epoch_store/sidebar-items.js
+++ crates/sui_core/authority/authority_per_epoch_store/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["EPOCH_DB_PREFIX"],"struct":["AuthorityEpochTables","AuthorityEpochTablesConfigurator","AuthorityEpochTablesReadOnly","AuthorityPerEpochStore","CertLockGuard","CertTxGuard","ExecutionComponents","ExecutionIndicesWithHash"]};
+window.SIDEBAR_ITEMS = {"constant":["EPOCH_DB_PREFIX"],"enum":["ConsensusCertificateResult"],"struct":["AuthorityEpochTables","AuthorityEpochTablesConfigurator","AuthorityEpochTablesReadOnly","AuthorityPerEpochStore","CertLockGuard","CertTxGuard","ExecutionComponents","ExecutionIndicesWithHash"]};
