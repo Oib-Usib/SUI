eb9e65088204d6e56fac159207ff33d91676762f
sadhansood <sadhansood@users.noreply.github.com>
2023-06-06 22:11:34 +0000

    deploy: d41072797247850771ed17d01ff1a300c986fc0d

--- crates/sui_storage/object_store/util/sidebar-items.js
+++ crates/sui_storage/object_store/util/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["copy_file","copy_files","copy_recursively","delete_files","delete_recursively","path_to_filesystem","put"]};
+window.SIDEBAR_ITEMS = {"fn":["copy_file","copy_files","copy_recursively","delete_files","delete_recursively","get","path_to_filesystem","put"]};
