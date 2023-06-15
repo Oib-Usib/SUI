10c8508676d403e9f0352187bc294b8f84179c92
Mark Logan <mark@mystenlabs.com>
2023-06-15 13:53:16 -0700

    wip

--- scripts/simtest/simtest-run.sh
+++ scripts/simtest/simtest-run.sh
@@ -2,16 +2,34 @@
 # Copyright (c) Mysten Labs, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+# Function to handle SIGINT signal (Ctrl+C)
+cleanup() {
+    echo "Cleaning up child processes..."
+    # Kill all child processes in the process group of the current script
+    kill -- "-$$"
+    exit 1
+}
+
+# Set up the signal handler
+trap cleanup SIGINT
+
 if [ -z "$NUM_CPUS" ]; then
   NUM_CPUS=$(cat /proc/cpuinfo | grep processor | wc -l) # ubuntu
 fi
 
 # filter out some tests that give spurious failures.
-TEST_FILTER="(not test(~cli_tests))"
+TEST_FILTER="(not (test(~cli_tests) or test(~batch_verification_tests)))"
 
 DATE=$(date +%s)
 SEED="$DATE"
-# LOG_FILE="log-$SEED"
+
+# create logs directory
+SIMTEST_LOGS_DIR=~/simtest_logs
+[ ! -d ${SIMTEST_LOGS_DIR} ] && mkdir -p ${SIMTEST_LOGS_DIR}
+[ ! -d ${SIMTEST_LOGS_DIR}/${DATE} ] && mkdir -p ${SIMTEST_LOGS_DIR}/${DATE}
+
+LOG_DIR="${SIMTEST_LOGS_DIR}/${DATE}"
+LOG_FILE="$LOG_DIR/log"
 
 # This command runs many different tests, so it already uses all CPUs fairly efficiently, and
 # don't need to be done inside of the for loop below.
@@ -20,20 +38,19 @@ MSIM_TEST_SEED="$SEED" \
 MSIM_WATCHDOG_TIMEOUT_MS=60000 \
 MSIM_TEST_NUM=30 \
 scripts/simtest/cargo-simtest simtest \
-  --package sui \
+  --color always \
   --test-threads "$NUM_CPUS" \
+  --package sui \
   --package sui-core \
+  --package sui-archival \
+  --package sui-e2e-tests \
+  --package sui-surfer \
   --profile simtestnightly \
-  -E "$TEST_FILTER"
-
-# create logs directory
-SIMTEST_LOGS_DIR=~/simtest_logs
-[ ! -d ${SIMTEST_LOGS_DIR} ] && mkdir -p ${SIMTEST_LOGS_DIR}
-[ ! -d ${SIMTEST_LOGS_DIR}/${DATE} ] && mkdir -p ${SIMTEST_LOGS_DIR}/${DATE}
+  -E "$TEST_FILTER" 2>&1 | tee "$LOG_FILE"
 
 for SUB_SEED in `seq 1 $NUM_CPUS`; do
   SEED="$SUB_SEED$DATE"
-  LOG_FILE=${SIMTEST_LOGS_DIR}/${DATE}/"log-$SEED"
+  LOG_FILE="$LOG_DIR/log-$SEED"
   echo "Iteration $SUB_SEED using MSIM_TEST_SEED=$SEED, logging to $LOG_FILE"
 
   # --test-threads 1 is important: parallelism is achieved via the for loop
@@ -42,13 +59,15 @@ for SUB_SEED in `seq 1 $NUM_CPUS`; do
   MSIM_WATCHDOG_TIMEOUT_MS=60000 \
   SIM_STRESS_TEST_DURATION_SECS=300 \
   scripts/simtest/cargo-simtest simtest \
+    --color always \
     --package sui-benchmark \
     --test-threads 1 \
     --profile simtestnightly \
     -E "$TEST_FILTER" > "$LOG_FILE" 2>&1 &
 
-  grep -Hn FAIL "$LOG_FILE"
 done
 
 # wait for all the jobs to end
 wait
+
+grep -Hn FAIL "$LOG_DIR"/*
